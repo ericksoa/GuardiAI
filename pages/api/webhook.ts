a7ecdcf75bdda17096ce888b3773925c7660db4b
aaron <aaron@orgspace.io>
2023-04-09 17:13:40 -0700

    first shot from GPT-4 to post the comment back

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 import { analyzePullRequest } from '../../lib/pr-review-bot';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -6,7 +7,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       const prUrl = req.body.pull_request.html_url;
       const reviewMessage = await analyzePullRequest(prUrl);
-      res.status(200).json({ message: reviewMessage });
+
+      // Post the review feedback as a comment on the pull request
+      const issueUrl = req.body.pull_request.issue_url;
+      const commentUrl = `${issueUrl}/comments`;
+
+      const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+      const headers = {
+        Authorization: `token ${GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github+json',
+      };
+
+      await axios.post(commentUrl, { body: reviewMessage }, { headers });
+
+      res.status(200).json({ message: 'Review feedback posted successfully' });
     } catch (error) {
       res.status(500).json({ error: (error as Error).message });
     }
